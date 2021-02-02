fbc621c03fe8bd0d188237c0c94c0339ef942502
Erki Luik <luikerki@gmail.com>
2021-02-02 08:50:43 +0200

    firebase connection

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const router = require('./router');
+const app = express();
+const port = 3000;
 
 const corsOptions = {
     origin: '*'
 }
 
+// parse application/json
+app.use(bodyParser.json())
+
 app.use(cors(corsOptions));
 
-app.get('/cars', (req, res) => {
-    res.send([
-        {
-            "id": "abcd",
-            "title": "title",
-            "content": "content"
-        }
-    ])
-    
-})
+app.use('/', router);
 
-app.post('/car', (req, res, next, body) => {
 
-})
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./private-config/firebase-admin.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
---
+++ router.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const admin = require("firebase-admin");
+
+let router = express.Router()
+
+router.get('/cars', (req, res) => {
+
+  carGet().then((firestoreData) => {
+    res.send(firestoreData);
+  })
+
+})
+
+router.post('/car', (req, res, next) => {
+  carPOST(req.body).then((firestoreData) => {
+    res.send(firestoreData);
+  })
+})
+
+router.delete('/car/:id', (req, res, next) => {
+  carDELETE(req.params.id).then((firestoreData) => {
+    res.sendStatus(firestoreData);
+  })
+})
+
+const carGet = function () {
+  return new Promise(async function (resolve, reject) {
+    // #1 Read
+    const carCollection = admin.firestore().collection('cars');
+
+    // #2 Read
+    const docs = await carCollection.listDocuments();
+
+    let docPromises = [];
+    docs.forEach(async doc => {
+      // Add all document read promises into single array
+      docPromises.push(doc.get())
+    });
+
+    // #3 Read - multiple reads in parallel, x1 read per document
+    let docSnapshots = await Promise.all(docPromises);
+    let finalData = [];
+    docSnapshots.forEach(snap => {
+      // #4 Read - for each document snapshot read data
+      let snapshotData = snap.data();
+      snapshotData['id'] = snap.id;
+      finalData.push(snapshotData);
+    });
+
+    if (finalData.length > 0) {
+      resolve(finalData);
+    } else {
+      resolve();
+    }
+  });
+}
+
+const carPOST = function (body) {
+  return new Promise(async function (resolve, reject) {
+    const carCollection = admin.firestore().collection('cars');
+
+    const newDoc = await carCollection.add(body);
+    const docSnap = newDoc.get();
+    resolve((await docSnap).data());
+  });
+}
+
+const carDELETE = function(id) {
+  return new Promise(async function (resolve, reject) {
+    await admin.firestore().collection('cars').doc(id).delete();
+    resolve(200);
+  });
+}
+
+module.exports = router;
