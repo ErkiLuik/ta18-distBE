caf689576bc880b55741d4c59093b3c22d986667
Erki Luik <luikerki@gmail.com>
2021-02-02 09:31:16 +0200

    changed example url

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ admin.initializeApp({
   });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}/cars`)
 })
